8a660d2681007eb679f2bb351c7bf5d51d1861d9
Rafaelrok <rafaelvieirasantos2006@hotmail.com>
2021-02-21 02:35:10 -0300

    Created project version 1.0.1

---
+++ src/main/java/br/com/serviceapi/SttiServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.serviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttiServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttiServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serviceapi/SttiServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttiServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
