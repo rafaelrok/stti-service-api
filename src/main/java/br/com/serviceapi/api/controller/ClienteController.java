b01fee04997cf451e3d83fa7830261ad2f73129d
Rafaelrok <rafaelvieirasantos2006@hotmail.com>
2021-02-23 02:08:55 -0300

    V1.0.1 JPA create tabela cliente - utilizando o FlyWay e jakarta

--- src/main/java/br/com/serviceapi/api/controller/ClienteController.java
+++ src/main/java/br/com/serviceapi/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.serviceapi.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import br.com.serviceapi.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manger;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Rafael");
-		cliente1.setEmail("rafael@teste.com.br");
-		cliente1.setTelefone("21 96897-3443");
-		
-		var cliente2 = new Cliente();	
-		cliente2.setId(2L);
-		cliente2.setNome("Mayara");
-		cliente2.setEmail("mayara@teste.com.br");
-		cliente2.setTelefone("21 99183-0116");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manger.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
